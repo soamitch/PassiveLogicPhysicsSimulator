540e0eee575f0c83e0bf546806dcb8263d9f0e23
Mitch Henrie <u1053479@eng.utah.edu>
2021-02-22 00:37:00 -0700

    Code cleanup, commenting, and polish.

--- PassiveLogicPhysicsSimulator/PhysicsSim.cs
+++ PassiveLogicPhysicsSimulator/PhysicsSim.cs
@@ -22,6 +22,9 @@ namespace PassiveLogicPhysicsSimulator
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Toggle Elements for starting and stopping the simulation
+        /// </summary>
         private void ToggleSim()
         {
             RunProgram.Enabled = !RunProgram.Enabled;
@@ -34,6 +37,9 @@ namespace PassiveLogicPhysicsSimulator
             running = !running;
         }
 
+        /// <summary>
+        /// Starting a fresh simulation based on user input.
+        /// </summary>
         private void RunProgram_Click(object sender, EventArgs e)
         {
             ToggleSim();
@@ -41,7 +47,8 @@ namespace PassiveLogicPhysicsSimulator
 
             //convert values
             double energyOutWatts = decimal.ToDouble(EnergyOut.Value) * 1000;
-            double waterSpeed = decimal.ToDouble(WaterSpeed.Value);
+            double waterSpeedMinutes = decimal.ToDouble(WaterSpeed.Value);
+            double waterSpeedSeconds = waterSpeedMinutes / 60.0;
             //Capacities are in liters
             int pipeCapacity = decimal.ToInt32(PipeLength.Value) * 30;
             //1000 liters in a cubic meter
@@ -49,43 +56,48 @@ namespace PassiveLogicPhysicsSimulator
             double startingTemp = decimal.ToDouble(StartingTemp.Value);
 
             //The amount of energy going into a liter of water each time it passes through the heating element
-            double joulesPerLiter = energyOutWatts * (waterSpeed / 60.0);
+            double joulesPerLiter = energyOutWatts * waterSpeedSeconds;
             int numLiters = pipeCapacity + tankCapacity;
 
+            //each double in this array represents a liter of water
             double[] waterTemps = new double[numLiters];
 
+            //initialize water temperatures to starting temperature
             for(int i=0; i<numLiters; i++)
             {
                 waterTemps[i] = startingTemp;
             }
 
+            //Start new thread to allow stop button to work.
             new Thread(() =>
             {
-                int minutes = 0;
+                int minutes = 1;
                 int curr = 0;
                 int tankStart = pipeCapacity / 2;
                 int tankEnd = tankStart + tankCapacity;
                 while (running)
                 {
                     //Change in temp = (energy in)/(mass * thermal constant)
+                    //(modulo allows array to wrap around).
                     waterTemps[curr % waterTemps.Length] += joulesPerLiter / (GramsPerLiter * WaterSpecificHeat);
 
-                    if (curr % waterSpeed == 0) {
-                        minutes++;
+                    //curr can be thought of to represent how many liters have moved through the heating element
+                    //Thus if curr % water speed in minutes = 0, a minute has passed
+                    if (curr % waterSpeedMinutes == 0) {
                         double TankTemp = 0;
-                        //TODO: Average Values in Tank
+                        //Average Values in Tank
                         for (int j = tankStart; j < tankEnd; j++)
                         {
                             TankTemp += waterTemps[j % waterTemps.Length];
                         }
                         TankTemp /= tankCapacity;
 
-                        //TODO: output results for t=now
+                        //output results for t=now
                         Invoke(new Action(() => OutputText.AppendText("t = " + minutes + " minutes; Tank Temperature = " +
                             TankTemp + "°C" + Environment.NewLine)));
+                        minutes++;
                     }
 
-                    //modulo allows array to wrap around
                     tankStart++;
                     tankEnd++;
                     curr++;
@@ -93,6 +105,9 @@ namespace PassiveLogicPhysicsSimulator
             }).Start();
         }
 
+        /// <summary>
+        /// Force stop the simulation from user input
+        /// </summary>
         private void StopButton_Click(object sender, EventArgs e)
         {
             ToggleSim();
